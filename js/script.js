2c815f59ffbb3d8ef754d51c1b626cfe3c38358f
amelia-cahyani <ameliaa.cahyaniii@gmail.com>
2025-11-30 22:57:57 +0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const passwordInput = document.getElementById("password-input");
+const togglePassword = document.getElementById("togglePassword");
+
+togglePassword.addEventListener("click", () => {
+  const isHidden = passwordInput.type === "password";
+  passwordInput.type = isHidden ? "text" : "password";
+  togglePassword.classList.toggle("ph-eye");
+  togglePassword.classList.toggle("ph-eye-slash");
+});
+``
